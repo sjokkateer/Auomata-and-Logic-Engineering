ffec6242a2ed2055cc64524ddbbae58b9534f0a3
Remy Bos <remy90@hotmail.com>
2020-06-19 14:52:18 +0200

    PARSER TESTS: Add tests for invalid inputs

--- src/test/java/ale2/classes/Automaton/Regex/ParserTest.java
+++ src/test/java/ale2/classes/Automaton/Regex/ParserTest.java
@@ -1,12 +1,32 @@
 package ale2.classes.Automaton.Regex;
 
 import ale2.classes.Automaton.Diagram.StateDiagram;
+import ale2.classes.Automaton.Exceptions.ParenthesisMismatchException;
 import ale2.classes.Automaton.Exceptions.RegularExpressionException;
+import junitparams.Parameters;
 import org.junit.Test;
 
+import static junitparams.JUnitParamsRunner.$;
 import static org.junit.Assert.*;
 
 public class ParserTest {
+
+    @Test(expected = ParenthesisMismatchException.class)
+    @Parameters(method = "getParenthesisMissmatchStrings")
+    public void parse_nonMatchingNumberOfParenthesis_expectedParenthesisMismatchExceptionThrown(String inputString) throws RegularExpressionException {
+        // Arrange
+        Parser parser = new Parser();
+
+        // Act // Assert
+        StateDiagram stateDiagram = parser.parse(inputString);
+    }
+
+    private Object[] getParenthesisMissmatchStrings() {
+        return $(
+                $("((()"),
+                $("())")
+        );
+    }
 
     @Test
     public void parseExpressionShouldReturnAStateDiagramMatchingExpression() throws RegularExpressionException {
